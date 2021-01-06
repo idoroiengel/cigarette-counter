878ddf7bcfc006c61a60764250134f4fea0b30a8
idoroiengel <idoroiengel@gmail.com>
2021-01-06 13:19:02 +0200

    - added a test for inserting several cigarettes, and getting all of them from db. - minor changes to tests' descriptions.

--- test/cigarette_test.dart
+++ test/cigarette_test.dart
@@ -14,7 +14,7 @@ void main() {
       await database.close();
     });
 
-    test("add new cigarette", () async {
+    test("Create a cigarette object, and insert into db, and verify we get an id back", () async {
       Cigarette cigarette = new Cigarette(
           chainSmokingNum: 1,
           smokingContext: "smoking context",
@@ -22,6 +22,28 @@ void main() {
           reasonToSmoke: "reason to smoke");
       final newlyCreatedId = await database.addCigarette(cigarette);
       bool assumption = newlyCreatedId > 0;
+
+      expect(assumption, true);
+    }, tags: "cigarette");
+
+    test(
+        "Create two cigarette objects, and save into db, then try to get all cigarettes in db, and verify there are two existing in db",
+        () async {
+      Cigarette cigarette1 = new Cigarette(
+          chainSmokingNum: 1,
+          smokingContext: "smoking context",
+          timeOfSmoke: DateTime.now(),
+          reasonToSmoke: "reason to smoke");
+      Cigarette cigarette2 = new Cigarette(
+          chainSmokingNum: 1,
+          smokingContext: "smoking context",
+          timeOfSmoke: DateTime.now(),
+          reasonToSmoke: "reason to smoke");
+      await database.addCigarette(cigarette1);
+      await database.addCigarette(cigarette2);
+
+      final List<Cigarette> list = await database.getAllSmokedCigarettes();
+      bool assumption = list.length == 2;
 
       expect(assumption, true);
     }, tags: "cigarette");
