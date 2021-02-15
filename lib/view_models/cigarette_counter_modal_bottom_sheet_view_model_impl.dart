7b474c10c0b970e218f8c522906742b291f6f5f5
idoroiengel <idoroiengel@gmail.com>
2021-02-15 16:21:01 +0200

    - removed redundant view model methods.

--- lib/view_models/cigarette_counter_modal_bottom_sheet_view_model_impl.dart
+++ lib/view_models/cigarette_counter_modal_bottom_sheet_view_model_impl.dart
@@ -7,23 +7,6 @@ import 'package:my_cigarette_counter/view_models/main_view_model.dart';
 class CigaretteCounterModalBottomSheetViewModelImpl
     implements CigaretteCounterModalBottomSheetViewModel {
   @override
-  // TODO: implement smokingContextChoice
-  Sink get smokingContextChoice => throw UnimplementedError();
-
-  @override
-  // TODO: implement smokingReasonChoice
-  Sink get smokingReasonChoice => throw UnimplementedError();
-
-  @override
-  // TODO: implement statisticsChoice
-  Sink get statisticsChoice => throw UnimplementedError();
-
-  @override
-  // TODO: implement outputSmokingContext
-  Stream<List<SmokingContext>> get outputSmokingContext =>
-      Stream<List<SmokingContext>>.value(SmokingContext.values);
-
-  @override
   // TODO: implement outputRecentCigarettes
   Stream<List<Cigarette>> get outputRecentCigarettes =>
       MainViewModel.database.getAllSmokedCigarettes().asStream().take(4);
